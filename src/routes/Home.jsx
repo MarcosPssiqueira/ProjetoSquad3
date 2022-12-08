6c802fd325503627aa68f9c4bbdb76077f70a9d5
João Guilherme <113355002+JGuilheerm@users.noreply.github.com>
2022-12-08 09:33:54 -0300

    Add files via upload

---
+++ src/routes/Home.jsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+import {useState, useEffect} from 'react'
+
+import "./Home.css";
+
+const Home = () => {
+  const [posts, setPosts] = useState([])
+
+  const getPosts = async() => {
+    
+    try {
+
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts");
+
+      const data = response.data;
+      
+      setPosts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+  
+// NÃO FINALIZADO
+//   return <div>
+//     <h1>Reviews recentes</h1>
+//     { posts.length === 0 ? (
+//     <p>Carregando...</p>
+//     ) : (
+//       posts.map((post) => (
+//         <div className="post" key={post.id}>
+//           <h2>{post.title}</h2>
+//           <p>{post.body}</p>
+//           <Link to={`/posts/${post.id}`} className="btn"> Ler mais</Link>
+//         </div>
+//       )
+
+//     )};
+//   </div>
+// };
+
+
+export default Home;
---
+++ src/routes/QuemSomos.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const QuemSomos = () => {
+  return (
+    <div>QuemSomos</div>
+  )
+}
+
+export default QuemSomos
