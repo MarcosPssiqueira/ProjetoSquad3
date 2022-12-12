4deaac80b4fa23112ecccb4374cd203cda3ed585
João Guilherme <113355002+JGuilheerm@users.noreply.github.com>
2022-12-12 09:39:32 -0300

    Delete src directory

--- src/routes/QuemSomos.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const QuemSomos = () => {
-  return (
-    <div>QuemSomos</div>
-  )
-}
-
-export default QuemSomos
--- src/routes/TrabalheConosco.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TrabalheConosco = () => {
-  return (
-    <div>TrabalheConosco</div>
-  )
-}
-
-export default TrabalheConosco
